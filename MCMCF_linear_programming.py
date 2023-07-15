a50e87bb983e2d87eb17426be5dce6a706b64c8a
Alessandro Cavaglià <alessandro.cavaglia@di.uito.it>
2023-07-15 18:49:02 +0200

    First try for file reading

--- MCMCF_linear_programming.py
+++ MCMCF_linear_programming.py
@@ -1,8 +1,7 @@
 from pyomo.environ import *
 import pyomo.environ as pyo
+import fileReading
 
-NODE_COUNT=5
-COMMODITY_COUNT=2
 '''
 Node balances = [[value for each comodity] for each node]
 
@@ -10,57 +9,62 @@ edges = [[i,j] for each (i,j) in A ] (i,j) means that the edge goes from i to j
 Edge costs = dictionary, edge_cost[[i,j]]=val
 Edge capacities = dictionary, edge_capacities[[i,j]]=val
 '''
-def linear_programming_MCMCF(nodes_balances,edges,edge_costs,edge_capacities,nodes,commodities):
-    print(nodes_balances)
-    print(edge_costs)
-    print(edge_capacities)
 
-    #Parametri
+
+def linear_programming_MCMCF(nodes_balances, edges, edge_costs, edge_capacities, nodes, commodities):
+    #print(nodes_balances)
+    #print(edge_costs)
+    #print(edge_capacities)
+
+    # Parametri
     model = ConcreteModel()
-    model.nodes=nodes
-    model.commodities=commodities
-    model.nodes_balances=nodes_balances
-    model.edges=edges
-    model.edge_costs=edge_costs
-    model.edge_capacities=edge_capacities
-    model.i=[edge[0] for edge in edges ]
-    model.j=[edge[1] for edge in edges ]
+    model.commodities = commodities
+    model.nodes_balances = nodes_balances
+    model.edges = edges
+    model.edge_costs = edge_costs
+    model.edge_capacities = edge_capacities
+    model.i = set([e[0] for e in edges])
+    model.j = set([e[1] for e in edges])
 
     print(model.i)
     print(model.j)
 
 
-    #Variabile
-    model.x = Var(model.i, model.j,commodities, within=pyo.PositiveReals)
+    # Variabile
+    model.x = Var(model.i, model.j, commodities, within=pyo.PositiveReals)
 
-    #Funzione obiettivo
+    # Funzione obiettivo
     def obj_rule(model):
-        return (sum(model.x[i,j,k] for i in model.i for j in model.j for k in commodities))
+        return (sum(model.x[i, j, k] for i in model.i for j in model.j for k in commodities))
+
     model.obj = Objective(expr=obj_rule(model), sense=minimize)
 
-    #Vincoli
+    # Vincoli
     model.balances_constraint = ConstraintList()
     for k in commodities:
         for n in nodes:
-            entering_edges=[]
-            exiting_edges=[]
+            entering_edges = []
+            exiting_edges = []
             for edge in model.edges:
-                if edge[0]==n:
+                if edge[0] == n:
                     exiting_edges.append(edge)
-                if edge[1]==n:
+                if edge[1] == n:
                     entering_edges.append(edge)
+
             model.balances_constraint.add(
-                (sum(model.x[arch[0], arch[1],k] for arch in entering_edges) - sum(model.x[arch[0], arch[1],k] for arch in exiting_edges)) == (nodes_balances[n][k]))
+                (sum(model.x[arch[0], arch[1], k] for arch in entering_edges) - sum(
+                    model.x[arch[0], arch[1], k] for arch in exiting_edges)) == (nodes_balances[n][k]))
 
     model.bundle_constraint = ConstraintList()
     for edge in edges:
         model.balances_constraint.add(
-            sum(model.x[edge[0],edge[1],k] for k in commodities)<=model.edge_capacities[edge]
+            sum(model.x[edge[0], edge[1], k] for k in commodities) <= model.edge_capacities[edge]
         )
 
     opt = pyo.SolverFactory('cplex')
     opt.solve(model)
     print_solution(model)
+
 
 def print_solution(model):
     for k in model.commodities:
@@ -68,7 +72,11 @@ def print_solution(model):
         for edge in model.edges:
             print(edge[0], edge[1], k, "-", str(model.x[edge[0], edge[1], k].value))
 
+
 if __name__ == '__main__':
+    (node_count, commodites_count, nodes_balances, edges, edge_costs, edge_capacities) = fileReading.load_problem(
+        "datasets/cinca2.dat")
+    '''
     nodes_balances=[[0 for j in range(COMMODITY_COUNT)] for i in range(NODE_COUNT)]
     nodes_balances[0][0]=-1
     nodes_balances[4][0] = 1
@@ -82,7 +90,5 @@ if __name__ == '__main__':
         edge_costs[edge]=1
     edge_capacities=dict()
     for edge in edges:
-        edge_capacities[edge]=1
-
-    linear_programming_MCMCF(nodes_balances,edges,edge_costs,edge_capacities,range(NODE_COUNT),range(COMMODITY_COUNT))
-
+        edge_capacities[edge]=1'''
+    linear_programming_MCMCF(nodes_balances,edges,edge_costs,edge_capacities,range(1,node_count+1),range(commodites_count))
---
+++ fileReading.py
@@ -0,0 +1,105 @@
+def load_problem(problem_name):
+    num_comodities, num_nodes, num_arches, num_arches_mutual = 0, 0, 0, 0
+    with open(problem_name, "r") as file:
+        firstLine = file.readline().split("	")
+        num_nodes = int(firstLine[1])
+        num_arches = int(firstLine[2])
+        num_comodities = int(firstLine[3])
+        arc_cost = dict()
+        single_comodity_capacity = dict()
+        supply = dict()
+        mutual_capacities = dict()
+        start_node = dict()
+        end_node = dict()
+
+        values = getValues(file)
+        for h in range(num_comodities):
+            for i in range(num_arches):
+                if (len(values) == 0):
+                    values = getValues(file)
+                arc_cost[(h, i)] = float(values[0])
+                del values[0]
+        for h in range(num_comodities):
+            for i in range(num_arches):
+                if (len(values) == 0):
+                    values = getValues(file)
+                single_comodity_capacity[(h, i)] = float(values[0])
+                del values[0]
+        for h in range(num_comodities):
+            for i in range(1,num_nodes+1):
+                if (len(values) == 0):
+                    values = getValues(file)
+                supply[(h, i)] = float(values[0])
+                del values[0]
+        for i in range(num_arches):
+            if (len(values) == 0):
+                values = getValues(file)
+            mutual_capacities[i] = float(values[0])
+            del values[0]
+        for i in range(num_arches):
+            if (len(values) == 0):
+                values = getValues(file)
+            start_node[i] = float(values[0])
+            del values[0]
+            end_node[i] = float(values[0])
+            del values[0]
+
+        # Check for correct formulation con capacity
+        for i in range(num_arches):
+            for h in range(num_comodities):
+                if (h >= 1):
+                    if (single_comodity_capacity[(h, i)] != single_comodity_capacity[h - 1, i]):
+                        print("Error, multiple capacities for edge ", i)
+        # Check for correct formulation con costs
+        for i in range(num_arches):
+            for h in range(num_comodities):
+                if (h >= 1):
+                    if (arc_cost[(h, i)] != arc_cost[h - 1, i]):
+                        print("Error, multiple costs for edge ", i)
+
+        for k in range(num_comodities):
+            print(k)
+            print(sum(supply[k,i]for i in range(1,num_nodes+1)))
+
+
+        return convert_problem(num_nodes, num_arches, num_comodities, arc_cost, single_comodity_capacity, supply,
+                        mutual_capacities, start_node, end_node)
+
+
+
+def convert_problem(num_nodes, num_arches, num_comodities, arc_cost, single_comodity_capacity, supply,
+                    mutual_capacities, start_node, end_node):
+
+    nodes_balances = [[None]] #First element skipped since nodes are calculated from 1 to N
+    edges = []
+    edge_costs = dict()
+    edge_capacities = dict()
+
+    node_count = num_nodes
+    commodites_count = num_comodities
+
+    for i in range(1,num_nodes+1):
+        balances = []
+        for j in range(num_comodities):
+            balances.append(supply[(j, i)])
+        nodes_balances.append(balances)
+
+    for i in range(num_arches):
+        edge=(start_node[i],end_node[i])
+        edges.append(edge)
+        edge_costs[edge]=arc_cost[(1,i)]
+        edge_capacities[edge]=single_comodity_capacity[(1,i)]
+
+    return (node_count,commodites_count,nodes_balances,edges,edge_costs,edge_capacities)
+
+def getValues(file):
+    line = file.readline()
+    if (line == '\n'):
+        line = file.readline()
+    values = line.split("	")
+    for idx, val in enumerate(values):
+        if (val == ''):
+            del values[idx]
+        else:
+            values[idx] = values[idx].replace("\n", "")
+    return values
